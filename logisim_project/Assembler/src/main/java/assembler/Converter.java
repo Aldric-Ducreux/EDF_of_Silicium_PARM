dd5439ffedabe69c91488a37b8a9dd9ffc3f526c
Lydia Baraukova <livelinndy@gmail.com>
2019-01-07 12:41:02 +0100

    Fusionner les classes Converter et Main

--- logisim_project/Assembler/src/main/java/assembler/Converter.java
+++ logisim_project/Assembler/src/main/java/assembler/Converter.java
@@ -328,4 +328,10 @@ class Converter {
     private String decToBin(int dec, int bits) {
         return String.format("%" + bits + "s", Integer.toBinaryString(dec)).replace(' ', '0');
     }
+	
+	public static void main(String[] args) {
+        File fin = new File("data/AssemblerIn.txt");
+        File fout = new File("data/AssemblerOut.txt");
+        new Converter().convertCode(fin,fout);
+    }
 }
